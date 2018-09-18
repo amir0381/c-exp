60fe1222113545ea09aeb1d0e39a8c632ba72f63
amirali <amiralisalimi.313@gmail.com>
2018-09-18 23:36:51 +0430

    Add append func and use instead of strcat

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 char *cexpGen();
+void append(char *s, char c);
 
 char c_exp[] = "1";
 char expressions[2] = {'+', '-'};
@@ -21,27 +22,33 @@ char *cexpGen()
     {
         if ((c_exp[strlen(c_exp) - 1]) == '+' || (c_exp[strlen(c_exp) - 1]) == '-')
         {
-            strcat(c_exp, ((c_exp[strlen(c_exp) - 2]) + 1));
+            append(c_exp, ((c_exp[strlen(c_exp) - 2]) + 1));
             continue;
         }
 
         if (now = 1)
         {
-            strcat(c_exp, expressions[1]);
+            append(c_exp, expressions[1]);
             now++;
             cexpGen();
         }
 
         if (now = 2)
         {
-            strcat(c_exp, expressions[2]);
+            append(c_exp, expressions[2]);
             now++;
             cexpGen();
         }
 
         if (now = 3)
         {
-            strcat(c_exp, ((c_exp[strlen(c_exp) - 1]) + 1));
+            append(c_exp, ((c_exp[strlen(c_exp) - 1]) + 1));
         }
     }
+}
+
+void append(char *s, char c)
+{
+    s[strlen(s)] = c;
+    s[strlen(s) + 1] = '\0';
 }
