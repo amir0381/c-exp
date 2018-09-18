fe029e370b2ad08a2e72453eab71bff884e27eaa
amirali <amiralisalimi.313@gmail.com>
2018-09-18 19:52:47 +0430

    Beautify code

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
-char * cexpGen ();
+
+char *cexpGen();
+
 
 char c_exp[] = "1";
 char expressions[2] = {'+', '-'};
+
 
 int main()
 {
@@ -14,26 +17,31 @@ int main()
     return 0;
 }
 
-char * cexpGen () {
+
+char *cexpGen()
+{
     int now = 1;
     while (atoi(c_exp[ strlen(c_exp) - 1 ]) > 10)
     {
         if (atoi(c_exp[ strlen(c_exp) - 1 ]) == '+' || atoi(c_exp[ strlen(c_exp) - 1 ]) == '-') {
-            strcat(c_exp, ( ( atoi( c_exp[ strlen(c_exp) - 2 ] )) + 1 ));
+            strcat(c_exp,((atoi(c_exp[ strlen(c_exp) - 2 ])) + 1 ));
             continue;
         }
+
         if (now = 1) {
         strcat(c_exp, expressions[1]);
         now++;
         cexpGen();
         }
+
         if (now = 2) {
         strcat(c_exp, expressions[2]);
         now++;
         cexpGen();
         }
+        
         if (now = 3) {
-        strcat(c_exp, ( ( atoi(c_exp[ strlen(c_exp) - 1 ])) ) + 1 );
+        strcat(c_exp, atoi(c_exp[ strlen(c_exp) - 1 ]) + 1 );
         }
     }
 }
