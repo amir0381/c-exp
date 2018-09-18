624849da43efd0a1ee750d33e7c3d14547e78398
amirali <amiralisalimi.313@gmail.com>
2018-09-18 19:59:20 +0430

    Format code

--- main.c
+++ main.c
@@ -2,13 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-
 char *cexpGen();
-
 
 char c_exp[] = "1";
 char expressions[2] = {'+', '-'};
-
 
 int main()
 {
@@ -17,31 +14,34 @@ int main()
     return 0;
 }
 
-
 char *cexpGen()
 {
     int now = 1;
-    while (atoi(c_exp[ strlen(c_exp) - 1 ]) > 10)
+    while (atoi(c_exp[strlen(c_exp) - 1]) > 10)
     {
-        if (atoi(c_exp[ strlen(c_exp) - 1 ]) == '+' || atoi(c_exp[ strlen(c_exp) - 1 ]) == '-') {
-            strcat(c_exp,((atoi(c_exp[ strlen(c_exp) - 2 ])) + 1));
+        if (atoi(c_exp[strlen(c_exp) - 1]) == '+' || atoi(c_exp[strlen(c_exp) - 1]) == '-')
+        {
+            strcat(c_exp, ((atoi(c_exp[strlen(c_exp) - 2])) + 1));
             continue;
         }
 
-        if (now = 1) {
-        strcat(c_exp, expressions[1]);
-        now++;
-        cexpGen();
+        if (now = 1)
+        {
+            strcat(c_exp, expressions[1]);
+            now++;
+            cexpGen();
         }
 
-        if (now = 2) {
-        strcat(c_exp, expressions[2]);
-        now++;
-        cexpGen();
+        if (now = 2)
+        {
+            strcat(c_exp, expressions[2]);
+            now++;
+            cexpGen();
         }
 
-        if (now = 3) {
-        strcat(c_exp, atoi(c_exp[ strlen(c_exp) - 1 ]) + 1 );
+        if (now = 3)
+        {
+            strcat(c_exp, atoi(c_exp[strlen(c_exp) - 1]) + 1);
         }
     }
 }
