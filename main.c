02b5003125f3c7c758567e617bd1fdea572f5d24
amirali <amiralisalimi.313@gmail.com>
2018-09-18 19:17:53 +0430

    first setup - not tested

--- main.c
+++ main.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char * cexpGen ();
 
 int main()
 {
-    
+    cexpGen();
     return 0;
+}
+
+char * cexpGen () {
+    char *c_exp = "1";
+    char exps[2] = {'+', '-'};
+    int now = 1;
+    while (atoi(c_exp[ strlen(c_exp) - 1 ]) > 10)
+    {
+        if (atoi(c_exp[ strlen(c_exp) - 1 ]) == '+' || atoi(c_exp[ strlen(c_exp) - 1 ]) == '-') {
+            strcat(c_exp, ( ( atoi( c_exp[ strlen(c_exp) - 2 ] )) + 1 ));
+            continue;
+        }
+        if (now = 1) {
+        strcat(c_exp, exps[1]);
+        now++;
+        cexpGen();
+        }
+        if (now = 2) {
+        strcat(c_exp, exps[2]);
+        now++;
+        cexpGen();
+        }
+        if (now = 3) {
+        strcat(c_exp, ( ( atoi(c_exp[ strlen(c_exp) - 1 ])) ) + 1 );
+        }
+    }
+
 }
