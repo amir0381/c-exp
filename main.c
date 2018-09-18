6a16d4994379f72343ca45e32f8c76c736c45bd7
amirali <amiralisalimi.313@gmail.com>
2018-09-18 22:55:24 +0430

    Change the type of c_exp defining for debug ( c_exp[] )

--- main.c
+++ main.c
@@ -21,27 +21,27 @@ char *cexpGen()
     {
         if ((c_exp[strlen(c_exp) - 1]) == '+' || (c_exp[strlen(c_exp) - 1]) == '-')
         {
-            strcat(c_exp[strlen(c_exp)], (((c_exp[strlen(c_exp) - 2])) + 1));
+            strcat(c_exp, ((c_exp[strlen(c_exp) - 2]) + 1));
             continue;
         }
 
         if (now = 1)
         {
-            strcat(c_exp[strlen(c_exp)], expressions[1]);
+            strcat(c_exp, expressions[1]);
             now++;
             cexpGen();
         }
 
         if (now = 2)
         {
-            strcat(c_exp[strlen(c_exp)], expressions[2]);
+            strcat(c_exp, expressions[2]);
             now++;
             cexpGen();
         }
 
         if (now = 3)
         {
-            strcat(c_exp[strlen(c_exp)], (c_exp[strlen(c_exp) - 1]) + 1);
+            strcat(c_exp, ((c_exp[strlen(c_exp) - 1]) + 1));
         }
     }
 }
